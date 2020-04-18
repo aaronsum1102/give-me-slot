cd5ebd43e7927d8908a7cd7f45a7262b2644db4e
Shao Xiang Sum <shao.xiang-sum@xlent.se>
2020-04-18 11:01:36 +0800

    Project initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true
+  },
+
+  extends: ["plugin:vue/recommended"],
+
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+
+  rules: {
+    "no-console": "off",
+    "no-debugger": "off"
+  },
+
+  extends: ["plugin:vue/recommended"]
+};
